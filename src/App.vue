997fea32fbd00eed14ad5080a61838b4c1ad1cc4
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-12-16 18:50:51 +0530

    :tada: Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app" v-if="weatherData">
+    <Card
+      :name="weatherData.name"
+      :country="weatherData.sys.county"
+      :sunrise="weatherData.sys.sunrise"
+      :sunset="weatherData.sys.sunset"
+      :temp="weatherData.main.tmep"
+      :max_temp="weatherData.main.max_temp"
+      :min_temp="weatherData.main.min_temp"
+      :windSpeed="weatherData.wind.speed"
+      :weather_desc="weatherData.weather.description"
+      :humidity="weatherData.main.humidity"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+require("dotenv").config();
+import Card from "./components/WeatherCard";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+  data: () => {
+    return {
+      OPEN_WEATHER_API: "9e9d9499cf2a6979cddba99d7199569f",
+      weatherURL: "http://api.openweathermap.org/data/2.5/weather?",
+      weatherData: null,
+    };
+  },
+  methods: {
+    async getWeatherData() {
+      try {
+        const response = await fetch(
+          `${this.weatherURL}q=London&appid=${this.OPEN_WEATHER_API}`
+        );
+        const data = await response.json();
+        console.log(data);
+        this.weatherData = data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    getTempValue(tempValue) {
+      return tempValue - 273;
+    },
+  },
+  computed: {},
+  mounted() {
+    this.getWeatherData();
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="weather-card">{{ name }}</div>
+</template>
+
+<script>
+export default {
+  name: "weather-card",
+  props: {
+    name: String,
+    country: String,
+    temp: Number,
+    max_temp: Number,
+    min_temp: Number,
+    humidity: Number,
+    windSpeed: Number,
+    weather_desc: String,
+    sunrise: Number,
+    sunset: Number,
+  },
+};
+</script>
+
+<style></style>
